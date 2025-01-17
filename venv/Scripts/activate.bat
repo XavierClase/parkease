b65f98bf98f6d22699b99ff4c2ba47927ce112e4
JaviFigueroaVicente <javierfigueroa969@gmail.com>
2025-01-17 15:56:10 +0100

    commit flask

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\xampp\htdocs\DAW2\parkease\parkease\venv
+set VIRTUAL_ENV=D:\DAW2\M07 Python\Flask\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
