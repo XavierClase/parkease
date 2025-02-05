432beb1b3d452f6bfa22d4b728aa293651e48755
Xavier <xavierperezfp23@ibf.cat>
2025-02-05 20:38:20 +0100

    nose

---
+++ app/config.py
@@ -0,0 +1,6 @@
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:1234@localhost:3308/python'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = "CLAVE SEGURA"
+    
---
+++ main.py
@@ -0,0 +1,8 @@
+from app import create_app
+
+app = create_app()
+# Inicializar base de datos y ejecutar la aplicación
+if __name__ == "__main__":
+    
+
+    app.run(host='0.0.0.0', port=81, debug=True)
