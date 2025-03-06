826a4ac50b220da05bbe791fe425e121fc55a71b
JaviFigueroaVicente <javierfigueroa969@gmail.com>
2025-03-06 20:01:57 +0100

    config

--- app/config.py
+++ app/config.py
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-class Config:
-=======
-
 class Config:
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost:3306/python'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
->>>>>>> origin/alex
     SECRET_KEY = "CLAVE SEGURA"
     
